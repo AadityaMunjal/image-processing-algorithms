0f4f9ac7c86c39810be4cf6883c28a42b2bdf1ad
AadityaMunjal <aadityamunjal06@gmail.com>
2021-03-08 19:49:25 +0530

    add greyscale and binary_convert

---
+++ main.py
@@ -0,0 +1,18 @@
+def binary_convert(image, threshold: int):
+  threshold *= 3
+  for row in range(image.shape[0]):
+    for col in range(image.shape[1]):
+      colorsum = sum(image[row][col][i] for i in range(3))
+
+      if colorsum > threshold:
+        image[row][col] = [255, 255, 255]
+      else:
+        image[row][col] = [0, 0, 0]
+  return image
+
+def grayscale(image):
+  for row in range(image.shape[0]):
+    for col in range(image.shape[1]):
+      avg = sum(image[row][col][i] for i in range(3)) // 3
+      image[row][col] = [avg for _ in range(3)]
+  return image
