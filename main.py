ccb283b6d8fe4c999572a029bd050fae71aaa7d5
AadityaMunjal <aadityamunjal06@gmail.com>
2021-03-21 21:50:24 +0530

    new algo: convert to ascii symbols

--- main.py
+++ main.py
@@ -14,4 +14,29 @@ def grayscale(image):
     for col in range(image.shape[1]):
       avg = sum(image[row][col][i] for i in range(3)) // 3
       image[row][col] = [avg for _ in range(3)]
-      
+
+def convert_to_ascii(image):
+  import cv2 as cv
+  from colorama import Fore, Back, Style
+  image = cv.resize(image, (0, 0), fx=0.1, fy=0.1) 
+  newImage = "\n"
+  print(newImage)
+  asciiChars = [" ", ".", ";", "/", "$", "#", "@"] # ascending 
+
+  color_step_threshold = 255 // len(asciiChars)
+
+  for col in range(image.shape[0]):
+    for row in range(image.shape[1]):
+      color_avg = sum(image[col][row][i] for i in range(3)) // 3
+
+      for i in range(len(asciiChars)):
+        if color_avg < color_step_threshold * i:
+          symbol = asciiChars[i-1]
+          break
+        else: 
+          symbol = asciiChars[0]
+
+      newImage += symbol
+    newImage += "\n"
+
+  return newImage
