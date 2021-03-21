f423ea48d1edddab3e2e97bf3e1a2fb66c820aa8
AadityaMunjal <aadityamunjal06@gmail.com>
2021-03-21 21:51:06 +0530

    try for convert to ascii

--- try.py
+++ try.py
@@ -1,16 +1,21 @@
 import cv2 as cv 
-from main import grayscale, binary_convert
+from main import grayscale, binary_convert, convert_to_ascii 
 
 
 image = cv.imread("pics/Desert.jpg", -1)
 
-def show_image():
-  cv.imshow("image", image)
+def show_image(img):
+  cv.imshow("image", img)
   cv.waitKey(0)
   cv.destroyAllWindows()
 
 grayscale(image)
-show_image()
+show_image(image)
 
 binary_convert(image, 60)
-show_image()
+show_image(image)
+
+image = cv.imread("pics/Chrysanthemum.jpg")
+
+image = convert_to_ascii(image)
+print(image)
