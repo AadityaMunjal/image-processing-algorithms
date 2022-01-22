60a2b52f65a13d109ee76a73003508278ad87263
AadityaMunjal <aadityamunjal06@gmail.com>
2022-01-22 17:34:07 +0530

    Fix #1

--- algorithms/Pixilate.py
+++ algorithms/Pixilate.py
@@ -19,21 +19,31 @@ def get_size(x, y):
 def pixilate(image, scale=1):
     size = get_size(image.shape[0], image.shape[1]) * scale
 
-    for row in range(0, image.shape[0] - size, size):
-        for col in range(0, image.shape[1] - size, size):
+    for row in range(0, image.shape[0], size):
+        for col in range(0, image.shape[1], size):
 
             total_color = [0, 0, 0]
+
+            pixels_run = 0
             for x in range(size):
                 for y in range(size):
-
-                    current_color = image[row + x][col + y]
-                    total_color = [total_color[i] + current_color[i] for i in range(3)]
+                    try:
+                        current_color = image[row + x][col + y]
+                        total_color = [
+                            total_color[i] + current_color[i] for i in range(3)
+                        ]
+                        pixels_run += 1
+                    except IndexError:
+                        break
 
             # populate pixels
-            average_color = [c // size ** 2 for c in total_color]
+            average_color = [c // pixels_run for c in total_color]
             for _x in range(size):
                 for _y in range(size):
-                    image[row + _x][col + _y] = average_color
+                    try:
+                        image[row + _x][col + _y] = average_color
+                    except IndexError:
+                        break
 
 
 if __name__ == "__main__":
@@ -41,5 +51,5 @@ if __name__ == "__main__":
 
     image = cv.imread("pics/Desert.jpg", -1)
 
-    pixilate(image, scale=3)
+    pixilate(image, scale=2)
     show_image(image)
